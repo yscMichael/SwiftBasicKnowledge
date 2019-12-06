e3f3ddc53fab5a5a0841fdc38b944d3e20c09ed0
杨世川 <1434636181@qq.com>
2019-12-06 09:03:15 +0800

    Swift网络请求测试

--- swiftProject/swiftProject/LoginController/LoginController.swift
+++ swiftProject/swiftProject/LoginController/LoginController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import SnapKit
+import Alamofire
 
 class LoginController: UIViewController {
     //MARK:属性
@@ -58,25 +59,51 @@ extension LoginController{
         print("点击跳转按钮")
         //self.navigationController?.pushViewController(HomeListController.init(), animated: true)
         
-        //测试获取验证码请求
-        //url
-        let getCodeString = "/api/skyworth-northbound/users/captcha"
-        //参数
+        //测试Alamofire
+        let baseURL = "http://58.251.35.131:80/api/skyworth-northbound/users/captcha"
         let param = ["phone":"19924535784"]
-        let netTool = SPCNetWorkManager.shared()
-        //成功block
-        let successBlock = {(result : AnyObject) -> () in
-            print("成功回调")
-            print(result)
-        }
-        //失败block
-        let failureBlock = {(error : Error?) -> () in
-            print("失败回调")
-            print(error ?? "失败")
-        }
-        //正式网络请求
-        netTool?.startRequest(withUrl: getCodeString, method: HTTPMethod.post, params: param, withSuccessBlock: successBlock as? (Any?) -> Void, withFailurBlock: failureBlock)
+        var jsonString = getJSONStringFromDictionary(dictionary: param as NSDictionary)
         
+        //头部信息
+        let headers: HTTPHeaders = [
+            "Accept": "application/json",
+            "Content-Type":"application/json",
+            "Authorization":"Basic c2t5d29ydGhkaWdpdGFsOnNreXdvcnRoZGlnaXRhbF9zZWNyZXQ="
+        ]
+
+        
+        Alamofire.request(baseURL, method: HTTPMethod.post, parameters: jsonString, encoding: URLEncoding.httpBody, headers: headers).responseJSON { (response) in
+            print("请求成功")
+            print(response)
+        }
+        
+//        //测试获取验证码请求(swift调用OC代码)
+//        //url
+//        let getCodeString = "/api/skyworth-northbound/users/captcha"
+//        //参数
+//        let param = ["phone":"19924535784"]
+//        let netTool = SPCNetWorkManager.shared()
+//        //成功block
+//        let successBlock = {(result : AnyObject) -> () in
+//            print("成功回调")
+//            print(result)
+//        }
+//        //失败block
+//        let failureBlock = {(error : Error?) -> () in
+//            print("失败回调")
+//            print(error ?? "失败")
+//        }
+//        //正式网络请求
 //        netTool?.startRequest(withUrl: getCodeString, method: HTTPMethod.post, params: param, withSuccessBlock: successBlock as? (Any?) -> Void, withFailurBlock: failureBlock)
+    }
+    
+    func getJSONStringFromDictionary(dictionary:NSDictionary) -> String {
+        if (!JSONSerialization.isValidJSONObject(dictionary)) {
+            print("无法解析出JSONString")
+            return ""
+        }
+        let data : NSData! = try? JSONSerialization.data(withJSONObject: dictionary, options: []) as NSData?
+        let JSONString = NSString(data:data as Data,encoding: String.Encoding.utf8.rawValue)
+        return JSONString! as String
     }
 }
