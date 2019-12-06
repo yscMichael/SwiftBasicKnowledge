a0244a8407ee7b477d3e81c802dce9f01d28b537
杨世川 <1434636181@qq.com>
2019-12-06 10:59:20 +0800

    Swift版的验证码和登陆接口正常

--- swiftProject/swiftProject/LoginController/LoginController.swift
+++ swiftProject/swiftProject/LoginController/LoginController.swift
@@ -15,6 +15,13 @@ class LoginController: UIViewController {
     //跳转按钮
     private lazy var jumpButton:UIButton = UIButton()
     
+    @IBOutlet weak var getCodeButton: UIButton!
+    
+    @IBOutlet weak var codeTextField: UITextField!
+    
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("登陆界面")
@@ -22,6 +29,22 @@ class LoginController: UIViewController {
         initData()
         //初始化View
         initViews()
+        
+    }
+    
+    //MARK:点击获取验证码接口
+    @IBAction func clickGetCodeButton(_ sender: UIButton) {
+        clickGetCodeButtonEvent(button: sender);
+    }
+    
+    //MARk:点击登陆接口
+    @IBAction func clickLoginButton(_ sender: UIButton) {
+        clickLoginButtonEvent(button: sender);
+    }
+    
+    //MARK:跳转界面
+    @IBAction func clickJumpButton(_ sender: UIButton) {
+        clickJumpButtonEvent(button: sender);
     }
 }
 
@@ -34,76 +57,78 @@ extension LoginController{
         print("打印oc信息")
         SPCPerson.eat()
     }
-    
     //初始化View
     private func initViews() {
         self.title = "登陆界面";
-        self.jumpButton.setTitle("跳转界面", for: UIControl.State.normal)
-        self.jumpButton.setTitleColor(UIColor.red, for: UIControl.State.normal)
-        self.jumpButton.backgroundColor = UIColor.lightText
-        self.jumpButton.addTarget(self, action: #selector(clickJumpButton), for: UIControl.Event.touchUpInside)
-        self.view.addSubview(self.jumpButton)
-
-        self.jumpButton.snp.makeConstraints { (make) in
-            make.width.equalTo(100)
-            make.height.equalTo(50)
-            make.center.equalTo(self.view)
-        }
     }
 }
 
 //MARK:Event
 extension LoginController{
-    //点击跳转按钮
-    @objc func clickJumpButton(sender:UIButton){
-        print("点击跳转按钮")
-        //self.navigationController?.pushViewController(HomeListController.init(), animated: true)
-        
-        //测试Alamofire
-        let baseURL = "http://58.251.35.131:80/api/skyworth-northbound/users/captcha"
-        let param = ["phone":"19924535784"]
-        var jsonString = getJSONStringFromDictionary(dictionary: param as NSDictionary)
-        
+    //MARK:点击获取验证码接口
+    func clickGetCodeButtonEvent(button :UIButton) {
+         //参数
+         let baseURL = "http://58.251.35.131:80/api/skyworth-northbound/users/captcha"
+         let param = ["phone":"19924535784"]
+         //头部信息
+         let headers: HTTPHeaders = [
+             "Accept": "application/json",
+             "Content-Type":"application/json",
+             "Authorization":"Basic c2t5d29ydGhkaWdpdGFsOnNreXdvcnRoZGlnaXRhbF9zZWNyZXQ="
+         ]
+        //请求
+         Alamofire.request(baseURL, method: HTTPMethod.post, parameters: param, encoding: JSONEncoding.default, headers: headers).responseJSON { (response) in
+             print("请求成功")
+             print(response)
+         }
+    }
+    //MARK:点击登陆接口
+    func clickLoginButtonEvent(button:UIButton) {
+        //参数
+        let baseURL = "http://58.251.35.131:80/api/skyworth-northbound/users/skyworthdigitallogin"
+        let param: [String: String] = ["username":"19924535784",
+                                       "password":self.codeTextField.text ?? "",
+                                       "grant_type":"password",
+                                       "scope":"all",
+                                       "type":"account"]
         //头部信息
         let headers: HTTPHeaders = [
             "Accept": "application/json",
             "Content-Type":"application/json",
             "Authorization":"Basic c2t5d29ydGhkaWdpdGFsOnNreXdvcnRoZGlnaXRhbF9zZWNyZXQ="
         ]
-
-        
-        Alamofire.request(baseURL, method: HTTPMethod.post, parameters: jsonString, encoding: URLEncoding.httpBody, headers: headers).responseJSON { (response) in
+        //请求
+        Alamofire.request(baseURL, method: HTTPMethod.post, parameters: param, encoding: JSONEncoding.default, headers: headers).responseJSON { (response) in
             print("请求成功")
             print(response)
         }
-        
-//        //测试获取验证码请求(swift调用OC代码)
-//        //url
-//        let getCodeString = "/api/skyworth-northbound/users/captcha"
-//        //参数
-//        let param = ["phone":"19924535784"]
-//        let netTool = SPCNetWorkManager.shared()
-//        //成功block
-//        let successBlock = {(result : AnyObject) -> () in
-//            print("成功回调")
-//            print(result)
-//        }
-//        //失败block
-//        let failureBlock = {(error : Error?) -> () in
-//            print("失败回调")
-//            print(error ?? "失败")
-//        }
-//        //正式网络请求
-//        netTool?.startRequest(withUrl: getCodeString, method: HTTPMethod.post, params: param, withSuccessBlock: successBlock as? (Any?) -> Void, withFailurBlock: failureBlock)
+    }
+
+    //MARK:点击跳转按钮
+    func clickJumpButtonEvent(button:UIButton){
+        print("点击跳转按钮")
+        self.navigationController?.pushViewController(HomeListController.init(), animated: true)
     }
     
-    func getJSONStringFromDictionary(dictionary:NSDictionary) -> String {
-        if (!JSONSerialization.isValidJSONObject(dictionary)) {
-            print("无法解析出JSONString")
-            return ""
+    //MARK:swift调用OC的网络请求
+    func swiftUseOCMethod() {
+        //测试获取验证码请求(swift调用OC代码)
+        //url
+        let getCodeString = "/api/skyworth-northbound/users/captcha"
+        //参数
+        let param = ["phone":"19924535784"]
+        let netTool = SPCNetWorkManager.shared()
+        //成功block
+        let successBlock = {(result : AnyObject) -> () in
+            print("成功回调")
+            print(result)
         }
-        let data : NSData! = try? JSONSerialization.data(withJSONObject: dictionary, options: []) as NSData?
-        let JSONString = NSString(data:data as Data,encoding: String.Encoding.utf8.rawValue)
-        return JSONString! as String
+        //失败block
+        let failureBlock = {(error : Error?) -> () in
+            print("失败回调")
+            print(error ?? "失败")
+        }
+        //正式网络请求
+        netTool?.startRequest(withUrl: getCodeString, method: HTTPMethod.post, params: param, withSuccessBlock: successBlock as? (Any?) -> Void, withFailurBlock: failureBlock)
     }
 }
